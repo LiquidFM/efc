63cedf23ed7d8cbaac20a903e8f27e4e741aeac0
Dmitriy Vilkov <dav.daemon@gmail.com>
2014-01-12 22:06:25 +0400

    Add EFC::Variant

---
+++ src/efc_variant.cpp
@@ -0,0 +1,467 @@
+/** @file efc_variant.cpp
+ *  @brief TODO: Put description here.
+ *
+ * TODO: Put description here.
+ *
+ * @copyright
+ * This file is part of the EFC library.
+ * @n@n
+ * Copyright (C) 2011-2014 Dmitriy Vilkov, <dav.daemon@gmail.com>
+ * @n@n
+ * the EFC library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * @n@n
+ * the EFC library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * @n@n
+ * You should have received a copy of the GNU General Public License
+ * along with the EFC library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "efc_variant.h"
+
+#include <brolly/assert.h>
+#include <platform/platform.h>
+#include <cstring>
+
+
+#if PLATFORM_CPU(LITTLE_ENDIAN)
+enum
+{
+    LoByte = 0,
+    LoWord = 0,
+    LoDWord = 0,
+    HiByte = sizeof(uint64_t) - 1,
+    HiWord = sizeof(uint64_t) / sizeof(uint16_t) - 1,
+    HiDWord = sizeof(uint64_t) / sizeof(uint32_t) - 1,
+};
+#else
+#error This platform is not supported
+#endif
+
+namespace EFC {
+
+typedef unsigned char byte_t;
+
+struct Variant::Private
+{
+    Private(uint8_t value) :
+        type(UINT8),
+        data()
+    { data.uint_value.uint8_value[LoByte] = value; }
+
+    Private(int8_t value) :
+        type(INT8),
+        data()
+    { data.int_value.int8_value[LoByte] = value; }
+
+    Private(uint16_t value) :
+        type(UINT16),
+        data()
+    { data.uint_value.uint16_value[LoWord] = value; }
+
+    Private(int16_t value) :
+        type(INT16),
+        data()
+    { data.int_value.int16_value[LoWord] = value; }
+
+    Private(uint32_t value) :
+        type(UINT32),
+        data()
+    { data.uint_value.uint32_value[LoDWord] = value; }
+
+    Private(int32_t value) :
+        type(INT32),
+        data()
+    { data.int_value.int32_value[LoDWord] = value; }
+
+    Private(uint64_t value) :
+        type(UINT64),
+        data()
+    { data.uint_value.uint64_value = value; }
+
+    Private(int64_t value) :
+        type(INT64),
+        data()
+    { data.int_value.int64_value = value; }
+
+    Private(float value) :
+        type(FLOAT),
+        data()
+    { data.float_value = value; }
+
+    Private(double value) :
+        type(DOUBLE),
+        data()
+    { data.double_value = value; }
+
+    Private(bool value) :
+        type(BOOL),
+        data()
+    { data.int_value.int8_value[LoByte] = value; }
+
+    Private(char value) :
+        type(CHAR),
+        data()
+    { data.int_value.int8_value[LoByte] = value; }
+
+    Private(const char *value, size_t size) :
+        type(STRING),
+        data()
+    {
+        ::memcpy(data.string_value.value, value, size);
+        data.string_value.value[data.string_value.size = size] = 0;
+    }
+
+    Private(const unsigned char *value, size_t size) :
+        type(BINARY),
+        data()
+    {
+        ::memcpy(data.string_value.value, value, size);
+        data.string_value.size = size;
+    }
+
+    struct StringData
+    {
+        size_t size;
+        char value[1];
+    };
+
+    Type type;
+    union UIntData
+    {
+        uint64_t uint64_value;
+        uint32_t uint32_value[HiDWord + 1];
+        uint16_t uint16_value[HiWord + 1];
+        uint8_t uint8_value[HiByte + 1];
+    };
+    union IntData
+    {
+        int64_t int64_value;
+        int32_t int32_value[HiDWord + 1];
+        int16_t int16_value[HiWord + 1];
+        int8_t int8_value[HiByte + 1];
+    };
+    union Data
+    {
+        UIntData   uint_value;
+        IntData    int_value;
+        float      float_value;
+        double     double_value;
+        StringData string_value;
+    } data;
+};
+
+Variant::Variant()
+{}
+
+Variant::Variant(uint8_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(int8_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(uint16_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(int16_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(uint32_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(int32_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(uint64_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(int64_t value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(float value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(double value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(bool value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(char value)
+{
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private)])
+        m_data.reset(new (buffer) Private(value));
+}
+
+Variant::Variant(const char *value)
+{
+    ASSERT(value);
+    size_t len = ::strlen(value);
+
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private) + len])
+        m_data.reset(new (buffer) Private(value, len));
+}
+
+Variant::Variant(const unsigned char *value, size_t size)
+{
+    ASSERT(value && size);
+
+    if (byte_t *buffer = new (std::nothrow) byte_t [sizeof(Private) + size - 1])
+        m_data.reset(new (buffer) Private(value, size));
+}
+
+Variant::~Variant()
+{}
+
+Variant::Type Variant::type() const
+{
+    ASSERT(isValid());
+    return m_data->type;
+}
+
+size_t Variant::size() const
+{
+    ASSERT(isValid());
+
+    switch (m_data->type)
+    {
+        case UINT8:
+            return sizeof(uint8_t);
+
+        case INT8:
+            return sizeof(int8_t);
+
+        case UINT16:
+            return sizeof(uint16_t);
+
+        case INT16:
+            return sizeof(int16_t);
+
+        case UINT32:
+            return sizeof(uint32_t);
+
+        case INT32:
+            return sizeof(int32_t);
+
+        case UINT64:
+            return sizeof(uint64_t);
+
+        case INT64:
+            return sizeof(int64_t);
+
+        case FLOAT:
+            return sizeof(float);
+
+        case DOUBLE:
+            return sizeof(double);
+
+        case BOOL:
+            return sizeof(bool);
+
+        case CHAR:
+            return sizeof(char);
+
+        case STRING:
+            return m_data->data.string_value.size - 1;
+
+        case BINARY:
+            return m_data->data.string_value.size;
+    }
+
+    return 0;
+}
+
+uint8_t Variant::asUint8() const
+{
+    ASSERT(isValid());
+    return m_data->data.uint_value.uint8_value[LoByte];
+}
+
+void Variant::setUint8(uint8_t value)
+{
+    (*this) = Variant(value);
+}
+
+int8_t Variant::asInt8() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int8_value[LoByte];
+}
+
+void Variant::setInt8(int8_t value)
+{
+    (*this) = Variant(value);
+}
+
+uint16_t Variant::asUint16() const
+{
+    ASSERT(isValid());
+    return m_data->data.uint_value.uint16_value[LoWord];
+}
+
+void Variant::setUint16(uint16_t value)
+{
+    (*this) = Variant(value);
+}
+
+int16_t Variant::asInt16() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int16_value[LoWord];
+}
+
+void Variant::setInt16(int16_t value)
+{
+    (*this) = Variant(value);
+}
+
+uint32_t Variant::asUint32() const
+{
+    ASSERT(isValid());
+    return m_data->data.uint_value.uint32_value[LoDWord];
+}
+
+void Variant::setUint32(uint32_t value)
+{
+    (*this) = Variant(value);
+}
+
+int32_t Variant::asInt32() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int32_value[LoDWord];
+}
+
+void Variant::setInt32(int32_t value)
+{
+    (*this) = Variant(value);
+}
+
+uint64_t Variant::asUint64() const
+{
+    ASSERT(isValid());
+    return m_data->data.uint_value.uint64_value;
+}
+
+void Variant::setUint64(uint64_t value)
+{
+    (*this) = Variant(value);
+}
+
+int64_t Variant::asInt64() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int64_value;
+}
+
+void Variant::setInt64(int64_t value)
+{
+    (*this) = Variant(value);
+}
+
+float Variant::asFloat() const
+{
+    ASSERT(isValid());
+    return m_data->data.float_value;
+}
+
+void Variant::setFloat(float value)
+{
+    (*this) = Variant(value);
+}
+
+double Variant::asDouble() const
+{
+    ASSERT(isValid());
+    return m_data->data.double_value;
+}
+
+void Variant::setDouble(double value)
+{
+    (*this) = Variant(value);
+}
+
+bool Variant::asBool() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int8_value[LoByte];
+}
+
+void Variant::setBool(bool value)
+{
+    (*this) = Variant(value);
+}
+
+char Variant::asChar() const
+{
+    ASSERT(isValid());
+    return m_data->data.int_value.int8_value[LoByte];
+}
+
+void Variant::setChar(char value)
+{
+    (*this) = Variant(value);
+}
+
+const char *Variant::asString() const
+{
+    ASSERT(isValid());
+    return m_data->data.string_value.value;
+}
+
+void Variant::setString(const char *value)
+{
+    (*this) = Variant(value);
+}
+
+const unsigned char *Variant::asBinary(size_t &size) const
+{
+    ASSERT(isValid());
+    size = m_data->data.string_value.size;
+    return reinterpret_cast<const unsigned char *>(m_data->data.string_value.value);
+}
+
+void Variant::setBinary(const unsigned char *value, size_t size)
+{
+    (*this) = Variant(value, size);
+}
+
+}
---
+++ src/efc_variant.h
@@ -0,0 +1,162 @@
+/** @file efc_variant.h
+ *  @brief TODO: Put description here.
+ *
+ * TODO: Put description here.
+ *
+ * @copyright
+ * This file is part of the EFC library.
+ * @n@n
+ * Copyright (C) 2011-2014 Dmitriy Vilkov, <dav.daemon@gmail.com>
+ * @n@n
+ * the EFC library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * @n@n
+ * the EFC library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * @n@n
+ * You should have received a copy of the GNU General Public License
+ * along with the EFC library. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EFC_VARIANT_H_
+#define EFC_VARIANT_H_
+
+#include <cstdint>
+#include <efc/SharedPointer>
+
+
+namespace EFC {
+
+class Variant
+{
+public:
+    enum Type
+    {
+        UINT8,
+        INT8,
+        UINT16,
+        INT16,
+        UINT32,
+        INT32,
+        UINT64,
+        INT64,
+        FLOAT,
+        DOUBLE,
+        BOOL,
+        CHAR,
+        STRING,
+        BINARY
+    };
+
+public:
+    Variant();
+    Variant(uint8_t value);
+    Variant(int8_t value);
+    Variant(uint16_t value);
+    Variant(int16_t value);
+    Variant(uint32_t value);
+    Variant(int32_t value);
+    Variant(uint64_t value);
+    Variant(int64_t value);
+    Variant(float value);
+    Variant(double value);
+    Variant(bool value);
+    Variant(char value);
+    Variant(const char *value);
+    Variant(const unsigned char *value, size_t size);
+    ~Variant();
+
+    bool isValid() const { return m_data.get() != NULL; }
+
+    Type type() const;
+    size_t size() const;
+
+    template <typename T>
+    inline void set(T value);
+
+    template <typename T>
+    inline T get();
+
+    uint8_t asUint8() const;
+    void setUint8(uint8_t value);
+
+    int8_t asInt8() const;
+    void setInt8(int8_t value);
+
+    uint16_t asUint16() const;
+    void setUint16(uint16_t value);
+
+    int16_t asInt16() const;
+    void setInt16(int16_t value);
+
+    uint32_t asUint32() const;
+    void setUint32(uint32_t value);
+
+    int32_t asInt32() const;
+    void setInt32(int32_t value);
+
+    uint64_t asUint64() const;
+    void setUint64(uint64_t value);
+
+    int64_t asInt64() const;
+    void setInt64(int64_t value);
+
+    float asFloat() const;
+    void setFloat(float value);
+
+    double asDouble() const;
+    void setDouble(double value);
+
+    bool asBool() const;
+    void setBool(bool value);
+
+    char asChar() const;
+    void setChar(char value);
+
+    const char *asString() const;
+    void setString(const char *value);
+
+    const unsigned char *asBinary(size_t &size) const;
+    void setBinary(const unsigned char *value, size_t size);
+
+private:
+    struct Private;
+    SharedPointer<Private> m_data;
+};
+
+
+template <> inline void Variant::set(uint8_t value) { setUint8(value); }
+template <> inline void Variant::set(int8_t value) { setInt8(value); }
+template <> inline void Variant::set(uint16_t value) { setUint16(value); }
+template <> inline void Variant::set(int16_t value) { setInt16(value); }
+template <> inline void Variant::set(uint32_t value) { setUint32(value); }
+template <> inline void Variant::set(int32_t value) { setInt32(value); }
+template <> inline void Variant::set(uint64_t value) { setUint64(value); }
+template <> inline void Variant::set(int64_t value) { setInt64(value); }
+template <> inline void Variant::set(float value) { setFloat(value); }
+template <> inline void Variant::set(double value) { setDouble(value); }
+template <> inline void Variant::set(bool value) { setBool(value); }
+template <> inline void Variant::set(char value) { setChar(value); }
+template <> inline void Variant::set(const char *value) { setString(value); }
+
+template <> inline uint8_t Variant::get<uint8_t>() { return asUint8(); }
+template <> inline int8_t Variant::get<int8_t>() { return asInt8(); }
+template <> inline uint16_t Variant::get<uint16_t>() { return asUint16(); }
+template <> inline int16_t Variant::get<int16_t>() { return asInt16(); }
+template <> inline uint32_t Variant::get<uint32_t>() { return asUint32(); }
+template <> inline int32_t Variant::get<int32_t>() { return asInt32(); }
+template <> inline uint64_t Variant::get<uint64_t>() { return asUint64(); }
+template <> inline int64_t Variant::get<int64_t>() { return asInt64(); }
+template <> inline float Variant::get<float>() { return asFloat(); }
+template <> inline double Variant::get<double>() { return asDouble(); }
+template <> inline bool Variant::get<bool>() { return asBool(); }
+template <> inline char Variant::get<char>() { return asChar(); }
+template <> inline const char *Variant::get<const char *>() { return asString(); }
+
+}
+
+#endif /* EFC_VARIANT_H_ */
