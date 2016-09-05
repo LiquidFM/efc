1695da898fe95eeb5ef712a8f92e31f399e2256a
Dmitriy Vilkov <dav.daemon@gmail.com>
2016-09-06 01:15:22 +0300

    Update 'Find' CMake script

--- cmake/FindEFC.cmake
+++ cmake/FindEFC.cmake
@@ -7,17 +7,28 @@
 #     EFC_LIB     - the library files
 
 
-find_path (EFC_INCLUDE
-           NAMES "efc/Atomics"
-           PATH_SUFFIXES include
-           DOC "EFC include directory")
+if (NOT EFC_FOUND)
+    find_path (EFC_INCLUDE
+	       NAMES "efc/Atomics"
+	       PATH_SUFFIXES include
+	       DOC "EFC include directory")
 
-find_library (EFC_LIB
-              NAMES efc
-              DOC "EFC library file")
+    find_library (EFC_LIB
+		  NAMES efc
+		  DOC "EFC library file")
 
-# handle the QUIETLY and REQUIRED arguments and set EFC_FOUND to TRUE if all listed variables are TRUE
-include (FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS (EFC DEFAULT_MSG EFC_INCLUDE EFC_LIB)
+    # handle the QUIETLY and REQUIRED arguments and set EFC_FOUND to TRUE if all listed variables are TRUE
+    include (FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS (EFC DEFAULT_MSG EFC_INCLUDE EFC_LIB)
 
-mark_as_advanced (EFC_INCLUDE EFC_LIB)
+    find_package (Platform REQUIRED)
+    list (APPEND EFC_INCLUDE ${PLATFORM_INCLUDE})
+
+    find_package (Brolly REQUIRED)
+    list (APPEND EFC_INCLUDE ${BROLLY_INCLUDE})
+    list (APPEND EFC_LIB ${BROLLY_LIB})
+
+    list (REMOVE_DUPLICATES EFC_INCLUDE)
+
+    mark_as_advanced (EFC_INCLUDE EFC_LIB)
+endif ()
